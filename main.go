24ed99423f4e1f444b8f3e486e47186db7411deb
fatemeh karimi <fatemehkarimi.001101@gamil.com>
2025-06-08 22:28:50 +0330

    feat(chronos_bot): Adds database scheme and queries

--- main.go
+++ main.go
@@ -1,5 +1,92 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/fatemehkarimi/chronos_bot/repository"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Database repository.DatabaseConfig
+}
+
+type Handler interface {
+	init() error
+}
+
+type Service struct {
+	Repo repository.Repository
+}
+
+func (s Service) init() error {
+	err := s.Repo.CreateTableFeatureFlag()
+
+	if err != nil {
+		return err
+	}
+
+	err = s.Repo.CreateTableSchedule()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadConfig() (Config, error) {
+	var cfg Config
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return cfg, err
+	}
+
+	err := viper.Unmarshal(&cfg)
+	return cfg, err
+
+}
+
 func main() {
+	config, err := LoadConfig()
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	connectionCredentials := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Database.Host,
+		config.Database.Port,
+		config.Database.User,
+		config.Database.Password,
+		config.Database.DBName,
+	)
+
+	db, err := sql.Open("postgres", connectionCredentials)
+	if err != nil {
+		slog.Error("failed to open db", slog.Any("err", err))
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		slog.Error("failed to ping database. err = ", slog.Any("err", err))
+		os.Exit(1)
+	}
+
+	postgersRepo := repository.PostgresRepository{DB: db}
+	service := Service{Repo: &postgersRepo}
+	err = service.init()
+
+	if err != nil {
+		slog.Error("failed to init service. error = ", slog.Any("err", err))
+		os.Exit(1)
+	}
 
 }
---
+++ repository/repository.go
@@ -0,0 +1,90 @@
+package repository
+
+import (
+	"database/sql"
+)
+
+type DatabaseConfig struct {
+	User     string
+	Password string
+	Host     string
+	Port     int
+	Name     string
+	DBName   string
+}
+
+type Repository interface {
+	CreateTableFeatureFlag() error
+	CreateTableSchedule() error
+	AddFeatureFlag(ownerId int, featureFlag string) error
+	AddSchedule(featureFlag, value string, calendarType CalendarType, year, month, day, hour, minute int) (int, error)
+	RemoveSchedule(scheduleId int) error
+}
+
+type PostgresRepository struct {
+	DB *sql.DB
+}
+
+type CalendarType int
+
+const (
+	_ CalendarType = iota
+	Shamsi
+	Qama
+)
+
+func CreateNewRepository(db *sql.DB) Repository {
+	return &PostgresRepository{
+		DB: db,
+	}
+}
+
+func (repo *PostgresRepository) CreateTableFeatureFlag() error {
+	query := `
+	CREATE TABLE IF NOT EXISTS feature_flag(
+		feature_flag VARCHAR PRIMARY KEY,
+		owner_id INT
+	);`
+
+	_, err := repo.DB.Exec(query)
+	return err
+}
+
+func (repo *PostgresRepository) CreateTableSchedule() error {
+	query := `
+	CREATE TABLE IF NOT EXISTS schedule(
+		schedule_id SERIAL PRIMARY KEY,
+		feature_flag VARCHAR REFERENCES feature_flag(feature_flag),
+		value TEXT,
+		calendar_type SMALLINT,
+		year INT,
+		month INT,
+		day INT,
+		hour INT,
+		minute INT
+);`
+	_, err := repo.DB.Exec(query)
+	return err
+}
+
+func (repo *PostgresRepository) AddFeatureFlag(ownerId int, featureFlag string) error {
+	query := `INSERT INTO feature_flag(owner_id, feature_flag) VALUES ($1, $2);`
+	_, err := repo.DB.Exec(query, ownerId, featureFlag)
+	return err
+}
+
+func (repo *PostgresRepository) AddSchedule(featureFlag, value string, calendarType CalendarType, year, month, day, hour, minute int) (int, error) {
+	query := `
+	INSERT INTO schedule(feature_flag, value, calendar_type, year, month, day, hour, minute) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+	var scheduleId int
+	err := repo.DB.QueryRow(query).Scan(&scheduleId)
+	return scheduleId, err
+}
+
+func (repo *PostgresRepository) RemoveSchedule(scheduleId int) error {
+	query := `
+	DELETE FROM schedule where schedule_id=$1
+	`
+	_, err := repo.DB.Exec(query)
+	return err
+}
