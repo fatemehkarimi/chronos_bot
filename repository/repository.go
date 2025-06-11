8b871ec4da853fd462de08f58fb9b30899d29104
fatemeh karimi <fatemehkarimi.001101@gamil.com>
2025-06-11 21:25:46 +0330

    fix(chronos_bot): Adds time to tables

--- repository/repository.go
+++ repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"time"
 )
 
 type DatabaseConfig struct {
@@ -43,7 +44,8 @@ func (repo *PostgresRepository) CreateTableFeatureFlag() error {
 	query := `
 	CREATE TABLE IF NOT EXISTS feature_flag(
 		feature_flag VARCHAR PRIMARY KEY,
-		owner_id INT
+		owner_id INT,
+		time TIMESTAMP
 	);`
 
 	_, err := repo.DB.Exec(query)
@@ -61,23 +63,24 @@ func (repo *PostgresRepository) CreateTableSchedule() error {
 		month INT,
 		day INT,
 		hour INT,
-		minute INT
+		minute INT,
+		time TIMESTAMP,
 );`
 	_, err := repo.DB.Exec(query)
 	return err
 }
 
 func (repo *PostgresRepository) AddFeatureFlag(ownerId int, featureFlag string) error {
-	query := `INSERT INTO feature_flag(owner_id, feature_flag) VALUES ($1, $2);`
-	_, err := repo.DB.Exec(query, ownerId, featureFlag)
+	query := `INSERT INTO feature_flag(owner_id, feature_flag, time) VALUES ($1, $2, $3);`
+	_, err := repo.DB.Exec(query, ownerId, featureFlag, time.Now())
 	return err
 }
 
 func (repo *PostgresRepository) AddSchedule(featureFlag, value string, calendarType CalendarType, year, month, day, hour, minute int) (int, error) {
 	query := `
-	INSERT INTO schedule(feature_flag, value, calendar_type, year, month, day, hour, minute) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+	INSERT INTO schedule(feature_flag, value, calendar_type, year, month, day, hour, minute, time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 	var scheduleId int
-	err := repo.DB.QueryRow(query).Scan(&scheduleId)
+	err := repo.DB.QueryRow(query, featureFlag, value, calendarType, year, month, day, hour, minute, time.Now()).Scan(&scheduleId)
 	return scheduleId, err
 }
 
@@ -85,6 +88,6 @@ func (repo *PostgresRepository) RemoveSchedule(scheduleId int) error {
 	query := `
 	DELETE FROM schedule where schedule_id=$1
 	`
-	_, err := repo.DB.Exec(query)
+	_, err := repo.DB.Exec(query, scheduleId)
 	return err
 }
